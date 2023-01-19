a0f0bbe5f94bb483f7cfdfdd9f5b8da8b3f17c74
pradeepsuthar <sutharpradeep081@gmail.com>
2023-01-19 20:23:50 +0530

    fix bugs

--- src/App.js
+++ src/App.js
@@ -13,22 +13,14 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 
 // react-router components
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 
 // @mui material components
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import Icon from "@mui/material/Icon";
-
-// Auto Genius React components
-import MDBox from "components/MDBox";
-
-// Auto Genius React example components
-// import Sidenav from "examples/Sidenav";
-import Configurator from "examples/Configurator";
 
 // Auto Genius React themes
 import theme from "assets/theme";
@@ -36,52 +28,17 @@ import theme from "assets/theme";
 // Material Dashboard 2 React Dark Mode themes
 import themeDark from "assets/theme-dark";
 
-// Material Dashboard 2 React routes
-// import routes from "routes";
+import "./App.css";
 
-// Material Dashboard 2 React contexts
-import { useMaterialUIController, setOpenConfigurator } from "context";
+import { useMaterialUIController } from "context";
 import SideNav2 from "examples/Sidenav2";
+import Leads from "layouts/leads";
 import Dashboard from "layouts/dashboard";
 
-// Images
-// import brandWhite from "assets/images/logo-ct.png";
-// import brandDark from "assets/images/logo-ct-dark.png";
-
 export default function App() {
-  const [controller, dispatch] = useMaterialUIController();
-  const {
-    // miniSidenav,
-    direction,
-    layout,
-    openConfigurator,
-    // sidenavColor,
-    // transparentSidenav,
-    // whiteSidenav,
-    darkMode,
-  } = controller;
+  const [controller] = useMaterialUIController();
+  const { direction, darkMode } = controller;
   const { pathname } = useLocation();
-  // Cache for the rtl
-  useMemo(() => {}, []);
-
-  // Open sidenav when mouse enter on mini sidenav
-  // const handleOnMouseEnter = () => {
-  //   if (miniSidenav && !onMouseEnter) {
-  //     setMiniSidenav(dispatch, false);
-  //     setOnMouseEnter(true);
-  //   }
-  // };
-
-  // Close sidenav when mouse leave mini sidenav
-  // const handleOnMouseLeave = () => {
-  //   if (onMouseEnter) {
-  //     setMiniSidenav(dispatch, true);
-  //     setOnMouseEnter(false);
-  //   }
-  // };
-
-  // Change the openConfigurator state
-  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
 
   // Setting the dir attribute for the body element
   useEffect(() => {
@@ -94,67 +51,17 @@ export default function App() {
     document.scrollingElement.scrollTop = 0;
   }, [pathname]);
 
-  // const getRoutes = (allRoutes) =>
-  //   allRoutes.map((route) => {
-  //     if (route.collapse) {
-  //       return getRoutes(route.collapse);
-  //     }
-
-  //     if (route.route) {
-  //       return <Route exact path={route.route} element={route.component} key={route.key} />;
-  //     }
-
-  //     return null;
-  //   });
-
-  const configsButton = (
-    <MDBox
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      width="3.25rem"
-      height="3.25rem"
-      bgColor="white"
-      shadow="sm"
-      borderRadius="50%"
-      position="fixed"
-      right="2rem"
-      bottom="2rem"
-      zIndex={99}
-      color="dark"
-      sx={{ cursor: "pointer" }}
-      onClick={handleConfiguratorOpen}
-    >
-      <Icon fontSize="small" color="inherit">
-        settings
-      </Icon>
-    </MDBox>
-  );
-
   return (
     <ThemeProvider theme={darkMode ? themeDark : theme}>
       <CssBaseline />
-      {layout === "dashboard" && (
-        <>
-          <SideNav2 />
-          {/* <Sidenav
-            color={sidenavColor}
-            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Auto Genius"
-            routes={routes}
-            onMouseEnter={handleOnMouseEnter}
-            onMouseLeave={handleOnMouseLeave}
-          /> */}
-          <Configurator />
-          {configsButton}
-        </>
-      )}
-      {layout === "vr" && <Configurator />}
-      <Routes>
-        {/* {getRoutes(routes)} */}
-        <Route exact path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="*" element={<Navigate to="/dashboard" />} /> */}
-      </Routes>
+      <div className="dFlex">
+        <SideNav2 />
+        <Routes>
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/leads" element={<Leads />} />
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </div>
     </ThemeProvider>
   );
 }
--- src/examples/Sidenav2/index.js
+++ src/examples/Sidenav2/index.js
@@ -5,18 +5,16 @@ import items from "./data/sidebar.json";
 // { color, brand, brandName, routes, onMouseEnter, onMouseLeave }
 const SideNav2 = () => {
   return (
-    <div className="main">
-      <div className="sidebar-container">
-        <div className="sidebar">
-          <div className="sidebar-header">
-            <i className="bi-info-circle-fill" />
-            <h1>Auto Genius</h1>
-          </div>
-          <div className="sidebar-buttons">
-            {items.map((item, index) => (
-              <SidebarItem key={index} item={item} />
-            ))}
-          </div>
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <i className="bi-info-circle-fill" />
+          <h1>Auto Genius</h1>
+        </div>
+        <div className="sidebar-buttons">
+          {items.map((item, index) => (
+            <SidebarItem item={item} key={index} />
+          ))}
         </div>
       </div>
     </div>
