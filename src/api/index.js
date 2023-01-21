8a8702bb17d24b746ea45b04a50f1b1d57d456c0
= <=>
2023-01-21 11:24:16 +0530

    Fetch API

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const baseUrl = "https://";
-const token = "";
+const baseUrl = "https://aaiina.app/api/";
+const token = "829416af2aac42568f2ca872fd172bd9";
 export async function getExecutive() {
   try {
     const data = await axios.get(baseUrl + "GetExecutive", {
