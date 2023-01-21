3fcdeb77549750519713c86aa7f3dffb2b291529
pradeepsuthar <sutharpradeep081@gmail.com>
2023-01-21 13:44:24 +0530

    make some changes

--- src/examples/Sidenav2/index.js
+++ src/examples/Sidenav2/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import SidebarItem from "./SidebarItem";
 import "./style.css";
 import items from "./data/sidebar.json";
+
+import Logo from "../../assets/images/logos/logo-large.png";
+
 // { color, brand, brandName, routes, onMouseEnter, onMouseLeave }
 function SideNav2() {
   return (
     <div className="sidebar-container">
       <div className="sidebar">
         <div className="sidebar-header">
-          <h1 className="mx-2">Auto Genius</h1>
+          <h1 className="mx-2">
+            <img src={Logo} width="120px" />
+          </h1>
         </div>
         <div className="sidebar-buttons">
           {items && items.map((item) => <SidebarItem item={item} />)}
